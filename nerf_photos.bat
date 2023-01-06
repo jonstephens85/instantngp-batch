64ffb4c6e35b3529d081e226c0dc2855e9462b5c
Jonathan Stephens <82414293+jonstephens85@users.noreply.github.com>
2023-01-06 08:38:33 -0800

    Add files via upload

---
+++ nerf_photos.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Get the folder path of the dropped folder
+set folderpath=%~1
+
+rem Get parent folder of the images folder
+for %%i in ("%~dp1") do set "parent_folder=%%~fi"
+
+rem Call the colmap2nerf python script, passing the folder path as an argument
+rem Modify --colmap_matcher to "sequential" if you images are taken in spatial sequence
+python "%~dp0\scripts\colmap2nerf.py" --colmap_matcher exhaustive --run_colmap --aabb_scale 16 --images "%folderpath%"
+
+%~dp0\instant-ngp.exe %parent_folder%\transforms.json
---
+++ nerf_video.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Get the file path of the dropped file
+set filepath=%~1
+
+rem Set the root_folder variable as the root folder of the dropped file
+set root_folder=%~dp1
+
+rem Prompt the user for the FPS value
+set /p fps=Enter FPS value: 
+
+rem Call the Python script, passing the file path as an argument
+python "%~dp0\scripts\colmap2nerf.py" --video_in "%filepath%" --video_fps %fps% --run_colmap --aabb_scale 16
+
+%~dp0\instant-ngp.exe %root_folder%\transforms.json
